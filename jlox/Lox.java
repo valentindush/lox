cdcc0efc39d86f03d07e5d7d8e63e960eb55d120
valentindush <codesvalentin@gmail.com>
2023-11-01 22:11:36 +0200

    upd: run

--- jlox/Lox.java
+++ jlox/Lox.java
@@ -13,10 +13,41 @@ public class Lox{
         if(args.length > 1){
             System.out.println('Usage: jlox [script]');
             System.exit(64);
+
         }else if(args.length == 1){
+
             runFile(args[0]);
+
         }else{
             runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException{
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    private static void runPrompt() throws IOException{
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for(;;){
+            System.out.print("> ");
+            String line  = reader.readLine();
+
+            if(line == null) break;
+            run(line);
+        }
+    }
+
+    private static void run(String source){
+        Scanner scanner = new Scanner(source);
+
+        List<Token> tokens = Scanner.scanTokens();
+
+        for(Token token : tokens){
+            System.out.println(token);
         }
     }
 }
